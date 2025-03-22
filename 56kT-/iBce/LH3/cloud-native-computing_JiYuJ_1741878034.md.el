fc4a4d1e1dedef9405873eb7361cdff1c3e1c878
ab123nER <00.limps.motive@icloud.com>
2025-03-22 16:40:13 +0100

    Fix a bug where the file fails to save changes

--- 56kT-/iBce/LH3/cloud-native-computing_JiYuJ_1741878034.md.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((flycheck-pylintrc . "pylintrc")))
- (nil . ((flycheck-gcc-language-standard . "c++2a")))
- (nil . ((flycheck-clang-language-standard . "c++2a"))))
